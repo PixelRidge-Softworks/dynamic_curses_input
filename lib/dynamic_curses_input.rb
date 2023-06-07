532b1d2cd4e51a29a38bf887141f2bda38f6be1c
VetheonGames <ceo@pixelatedstudios.net>
2023-06-07 15:56:30 -0600

    initial commit

---
+++ lib/dynamic_curses_input.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "dynamic_curses_input/version"
+
+module DynamicCursesInput
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dynamic_curses_input/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module DynamicCursesInput
+  VERSION = "0.1.0"
+end
